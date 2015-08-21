9a1d2780ac20906de1ff5f8ca3d802070ac28be4
Ádám Gólya <adam.stork@gmail.com>
2015-08-21 10:36:43 +0200

    feat(app): add ssh poc
    Signed-off-by: Ádám Gólya <adam.stork@gmail.com>

--- app.js
+++ app.js
@@ -10,12 +10,18 @@ var logger = require('koa-logger');
 var serve = require('koa-static');
 var router = require('koa-router')();
 var koa = require('koa');
+var http = require('http');
+var https = require('https');
+//var forceSSL = require('koa-force-ssl');
+
+var fs = require('fs');
 var path = require('path');
 var app = module.exports = koa();
 
 
 app.keys = ['0ef7803e-473a-11e5-9d62-126b7daeca32'];
 
+//app.use(forceSSL());
 app.use(logger());
 app.use(bodyParser());
 app.use(session(app));
@@ -70,8 +76,17 @@ app.use(serve(path.join(__dirname, 'public/www')));
 app.use(compress());
 
 if (!module.parent) {
-  app.listen(3000);
-  console.log('listening on port 3000');
+    // SSL options
+    var options = {
+        key: fs.readFileSync('./test_certs/test.key'),
+        cert: fs.readFileSync('./test_certs/test.cert')
+    };
+
+    var port = 3000;
+    //app.listen(port);
+    http.createServer(app.callback()).listen(port);
+    //https.createServer(options, app.callback()).listen(443);
+    console.log('listening on port', port);
 }
 
 router.get('/', authed);
